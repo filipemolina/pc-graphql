1a07a75eec187f0574025e7e116fceeb053cf772
Filipe Molina <filipe.molina@permissionclick.com>
2021-03-18 00:12:38 -0500

    PC-GRAPHQL: Adds Babel, Jest, and the first unit tests

---
+++ utils/utils.spec.ts
@@ -0,0 +1,52 @@
+import {
+  getQuestionsById,
+  getOrganizationById,
+  getDocumentsByOrganizationId,
+} from './';
+import { mock_data } from '../mock_data';
+
+describe('The util function tests for GraphQL server', () => {
+  const { questions, documents, organizations } = mock_data;
+
+  describe('The getQuestionsById function', () => {
+    it("Should return the correct questions when valid ID's are passed", () => {
+      const result = getQuestionsById(questions, ['1', '3']);
+      const expected = [questions[0], questions[2]];
+
+      expect(result).toEqual(expected);
+    });
+
+    it("Should return an empty array if invalid ID's are passed", () => {
+      const result = getQuestionsById(questions, ['55', '99']);
+      const expected = [];
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('The getOrganizationById function', () => {
+    it('Should return a valid Organization if a valid ID is passed', () => {
+      const result = getOrganizationById(organizations, '1');
+      const expected = organizations[0];
+
+      expect(result).toEqual(expected);
+    });
+
+    it('Should return undefined if an invalid ID is passed', () => {
+      const result = getOrganizationById(organizations, '789');
+
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe('The getDocumentsByOrganizationId function', () => {
+    it('Should return an array of documents if a valid organization ID is passed', () => {
+      const result = getDocumentsByOrganizationId(documents, '1');
+      const expected = documents.filter(
+        (document) => document.organizationId === '1'
+      );
+
+      expect(result).toEqual(expected);
+    });
+  });
+});
